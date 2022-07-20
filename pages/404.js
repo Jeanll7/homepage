59f963df536d6e8aba94bba0ce2e4765da2105e2
Jean Leal <64782576+Jeanll7@users.noreply.github.com>
2022-07-20 01:23:42 -0300

    title error

--- pages/404.js
+++ pages/404.js
@@ -12,32 +12,34 @@ import {
 
 const NotFound = () => {
     return (
-        <Container>
-            <Box>
-                <Center>
-                    <Image
-                        mt={8}
-                        mb={8}
-                        borderStyle="solid"
-                        maxWidth="160px"
-                        display="flex"
-                        borderRadius="full"
-                        src="/images/emoji.png"
-                        alt="Image Banner"
-                    />
-                </Center>
-            </Box>
-            <Heading as="h1" align="center">404</Heading>
-            <Heading fontSize='2xl' align="center">Não encontrada</Heading>
-            <Text align="center">A página que você está procurando não foi encontrada.</Text>  
-            <Divider my={6} />
+        <Layout title="Error">
+            <Container>
+                <Box>
+                    <Center>
+                        <Image
+                            mt={8}
+                            mb={8}
+                            borderStyle="solid"
+                            maxWidth="160px"
+                            display="flex"
+                            borderRadius="full"
+                            src="/images/emoji.png"
+                            alt="Image Banner"
+                        />
+                    </Center>
+                </Box>
+                <Heading as="h1" align="center">404</Heading>
+                <Heading fontSize='2xl' align="center">Não encontrada</Heading>
+                <Text align="center">A página que você está procurando não foi encontrada.</Text>  
+                <Divider my={6} />
 
-            <Box my={6} align="center">
-                <NextLink href="/">
-                    <Button coloScheme="teal">Voltar ao início</Button>
-                </NextLink>
-            </Box>
-        </Container>
+                <Box my={6} align="center">
+                    <NextLink href="/">
+                        <Button coloScheme="teal">Voltar ao início</Button>
+                    </NextLink>
+                </Box>
+            </Container>
+        </Layout>
     )
 }
 
